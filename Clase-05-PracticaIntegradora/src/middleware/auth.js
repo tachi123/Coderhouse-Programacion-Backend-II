bce07b2a0b4d68471a8695cc7b22be3e5750d2eb
Camilo <camilomedina.es@gmail.com>
2025-04-15 20:34:43 -0400

    Se hacen correcciones para que el codigo no de errores:
    en config: configure para abrir desde la carpeta src
    
    en auth.js modifiqué la comprobacion del token
    
    si la cookie no existe redirige y renderiza el login y detiene la ejecucion del middleware.
    
    en let token:
    si el valor de la cookie empieza con Bearer hace el split y toma la segunda parte osea el JWT real. si no, asume que el valor de la cookie es el token y lo asigna tal cual
    
    y en la ultima parte para configurar el loggedout si existe la cookie renderiza la ventana current y no las otras porque para que va a querer loggear si ya lo esta, si no existe la cookie el middleware sigue con el proceso de destruccion de la cookie
    
    en el router se agrega el valor del nombre al token para poder renderizarlo en la vista y despues poder mostrarlo en el current. se agrega el post para poder cerrar sesion.
    
    en view.router se tiene que pasar el objeto currentUser asignado a req.user para poder ver los nombres renderizados en la vista.

--- Clase-05-PracticaIntegradora/src/middleware/auth.js
+++ Clase-05-PracticaIntegradora/src/middleware/auth.js
@@ -1,28 +1,35 @@
-import jwt from 'jsonwebtoken';
-import config from '../config/config.js';
+import jwt from "jsonwebtoken";
+import config from "../config/config.js";
 
 const PRIVATE_KEY = config.JWT_PRIVATE_KEY;
 
 export const isLoggedIn = (req, res, next) => {
-    const authHeader = req.cookies.currentUser;
-    if(!authHeader) 
-        res.render('login'); //Redirijo al login si no hay sesión
+  const authHeader = req.cookies.currentUser;
+  if (!authHeader) {
+    return res.render("login");
+  }
 
-    const token = authHeader.split(' ')[1]; //Se hace el split para retirar la palabra 'Bearer'
-    jwt.verify(token, PRIVATE_KEY, (error, credentials) => {
-        //jwt verifica el token existente y corrobora si es un token válido, alterado, expirado, etc.
-        if(error) res.render('login'); //Redirijo al login si no hay sesión
-        //Si todo está en orden, se descrifra correctamente el token y se envía al usuario
-        req.user = credentials;
-        next();
-    })
-}
+  let token;
+  if (authHeader.startsWith("Bearer ")) {
+    token = authHeader.split(" ")[1];
+  } else {
+    token = authHeader;
+  }
+
+  jwt.verify(token, PRIVATE_KEY, (error, credentials) => {
+    if (error) {
+      return res.render("login");
+    }
+    req.user = credentials;
+    next();
+  });
+};
 
 export const isLoggedOut = (req, res, next) => {
-    const authHeader = req.cookies.currentUser;
-    if(!authHeader) {
-        next();
-    }else{
-        res.render('current');
-    }
-}
+  const authHeader = req.cookies.currentUser;
+  if (authHeader) {
+    res.render("current");
+  } else {
+    next();
+  }
+};
--- Clase-05-PracticaIntegradora/src/routes/view.router.js
+++ Clase-05-PracticaIntegradora/src/routes/view.router.js
@@ -1,15 +1,17 @@
-import { Router } from 'express';
-import { isLoggedIn, isLoggedOut } from '../middleware/auth.js';
+import { Router } from "express";
+import { isLoggedIn, isLoggedOut } from "../middleware/auth.js";
 
 const router = Router();
 
 //Endpoint para REGISTER
-router.get('/register', isLoggedOut, (req, res) => res.render('register'));
+router.get("/register", isLoggedOut, (req, res) => res.render("register"));
 
 //Endpoint para LOGIN
-router.get('/login', isLoggedOut, (req, res) => res.render('login'));
+router.get("/login", isLoggedOut, (req, res) => res.render("login"));
 
 //Endpoint para CURRENT  (usuario actual)
-router.get('/current', isLoggedIn, (req, res) => res.render('current'));
+router.get("/current", isLoggedIn, (req, res) =>
+  res.render("current", { currentUser: req.user })
+);
 
 export default router;
